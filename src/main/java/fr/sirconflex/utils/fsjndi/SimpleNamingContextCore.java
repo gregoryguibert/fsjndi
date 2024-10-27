f319c6d76d5b94a23bf5ec1b7518e0212a0d70ff
Gregory Guibert <gregory.guibert@gmail.com>
2024-10-27 14:39:31 +0100

    Use unsupported operation exception

--- src/main/java/fr/sirconflex/utils/fsjndi/SimpleNamingContextCore.java
+++ src/main/java/fr/sirconflex/utils/fsjndi/SimpleNamingContextCore.java
@@ -13,148 +13,122 @@ import javax.naming.NamingException;
 public abstract class SimpleNamingContextCore implements Context {
 	@Override
 	public Object lookup(Name name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void bind(Name name, Object obj) throws NamingException {
-		throwBecauseNotImplemented();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void rebind(Name name, Object obj) throws NamingException {
-		throwBecauseNotImplemented();
-		
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void unbind(Name name) throws NamingException {
-		throwBecauseNotImplemented();
-		
+		throw new UnsupportedOperationException();
 	}
-
 
 	@Override
 	public void rename(Name oldName, Name newName) throws NamingException {
-		throwBecauseNotImplemented();
-		
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void rename(String oldName, String newName) throws NamingException {
-		throwBecauseNotImplemented();
-		
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public NamingEnumeration<NameClassPair> list(Name name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public NamingEnumeration<NameClassPair> list(String name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public NamingEnumeration<Binding> listBindings(Name name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public NamingEnumeration<Binding> listBindings(String name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void destroySubcontext(Name name) throws NamingException {
-		throwBecauseNotImplemented();
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public void destroySubcontext(String name) throws NamingException {
-		throwBecauseNotImplemented();
-		
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Context createSubcontext(Name name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Context createSubcontext(String name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object lookupLink(Name name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object lookupLink(String name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public NameParser getNameParser(Name name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public NameParser getNameParser(String name) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Name composeName(Name name, Name prefix) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public String composeName(String name, String prefix) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object addToEnvironment(String propName, Object propVal) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Object removeFromEnvironment(String propName) throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public Hashtable<?, ?> getEnvironment() throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
 	public String getNameInNamespace() throws NamingException {
-		throwBecauseNotImplemented();
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
-	private Object throwBecauseNotImplemented() throws NamingException {
-		throw new NamingException("This method is not implemented");
-	}
 }
